fd8ee6b31839c42c199393f459fa906631a1f02d
Ferhan Akkan <>
2022-05-22 10:12:19 +0300

    Edit Package file

--- Package.swift
+++ Package.swift
@@ -5,24 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "AppCircleSampleProject",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AppCircleSampleProject",
             targets: ["AppCircleSampleProject"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppCircleSampleProject",
-            dependencies: []),
-        .testTarget(
-            name: "AppCircleSampleProjectTests",
-            dependencies: ["AppCircleSampleProject"]),
+            dependencies: [])
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
